ef4d88f92d0849b5b4b143070f8cd96dbb76ec6c
Mithat Kara <mithatkara93@gmail.com>
2022-01-29 14:22:26 +0300

    router-tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
+// react ile sayfalarımızın birbirine link vasıtası ile yönlenmesi Link component sağlar.
+import { Outlet, Route, Routes } from 'react-router';
+import UsersPage from './pages/UsersPage';
+import TodosPage from './pages/TodosPage';
+import Layout from './components/Layout';
+import { Container } from 'react-bootstrap';
+import UserDetailPage from './pages/UserDetailPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      {/* uygulama içerisinde hangi linkin hangi componente yönleneceği kuralını Routes Component içerisinde tanımlıyouruz. */}
+      {/* tüm yönledirmeleri Routes altında tutarız, Route ise ilgili link ile sayfaya yönlendiğimiz componentleri tanıttığımız Routing component */}
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="users" element={<UsersPage />} />
+          <Route path="todos" element={<TodosPage />} />
+          <Route path="user-detail/:id" element={<UserDetailPage />} />
+          {/* eğer yukarıdaki pathlerden birisi ile eşleşilemediği takdirde aşağıdaki route componente girecektir. */}
+          <Route path="*" element={<div>Sayfa bulunamadı</div>} />
+        </Route>
+      </Routes>
+    </Container>
   );
 }
 
+// :id veya :name ne istersek yazabiliriz. :value ile tanımladığımız route yapısına dinamik route diyoruz. buraya 1,2,3 gibi user'a ait id değerini gönderip sonra UserDetailPage componentinde bu id değerine ulaşacağız.
 export default App;
---
+++ src/pages/UsersPage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ListGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { UserData } from './UsersData';
+
+// page componentler sayfa componentleri olduğu için sayfa yönlendirmesi ile çalışır ve içerisinde birden fazla  react componenti barındırır.
+
+
+
+function UsersPage() {
+
+  return <ListGroup>
+    {
+      UserData.map((item, key) => {
+        return <ListGroup.Item key={key} >
+          <Link to={`/user-detail/${item.id}`}>
+            {item.name} {item.surname}
+          </Link>
+        </ListGroup.Item>
+      })
+    }
+
+  </ListGroup>
+
+}
+
+export default UsersPage;
+
+// users linkine tıklayınca yönleneceğimiz sayfa
